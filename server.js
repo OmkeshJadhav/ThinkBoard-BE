feee09b4214ace30b34544c836d3261ff065f53e
Omkesh <omkeshjadhav.dev@gmail.com>
2026-01-14 23:30:32 +0530

    GET route

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const PORT = 3000
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.status(200).send("You have 6 notes")
+})
+
 app.listen(PORT, () => {
     console.log(`App is listening on the PORT ${PORT}`);
 })
