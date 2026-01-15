140d5ef5c360f0c675985f766da930888442bc19
Omkesh <omkeshjadhav.dev@gmail.com>
2026-01-15 23:05:34 +0530

    Routing for PUT and delete api

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.post("/api/notes", (req, res) => {
     res.status(201).send("New Note created successfully.")
 })
 
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).send("Note updated successfully")
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).send("Note updated successfully")
+})
+
 app.listen(PORT, () => {
     console.log(`App is listening on the PORT ${PORT}`);
 })
