df19b8d00cfee20b11f5c6901c3818014b857fc2
Omkesh <omkeshjadhav.dev@gmail.com>
2026-01-23 09:14:52 +0530

    Create Schema and Model

---
+++ src/models/Note.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+// 1 - Create a schema using new mongoose.Schema
+// 2 - Create a model off that schema using mongoose.model("Model Name", schema)
+
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        constent: {
+            type: String,
+            required: true
+        }
+    }, 
+    { 
+        timestamps: true 
+    }
+)
+
+const Note = mongoose.model("Note", noteSchema)
+
+export default Note;
